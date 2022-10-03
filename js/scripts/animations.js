c4cc14a63ae29033983f242debf20b25d5dccf5e
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-10-03 14:23:47 +0100

    animations in cloud section using GSAP

--- js/scripts/animations.js
+++ js/scripts/animations.js
@@ -14,3 +14,38 @@ timeline
         x: '-15%',
         opacity: 0
     }, "<")
+
+// ScrollTrigger
+
+// Clouds
+gsap.to(".cloud-2", {
+    yPercent: -30,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".cloud-2",
+      // start: "top bottom", // the default values
+      // end: "bottom top",
+      scrub: true
+    }, 
+});
+gsap.to(".cloud-3", {
+    yPercent: -100,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".cloud-3",
+      // start: "top bottom", // the default values
+      // end: "bottom top",
+      scrub: true
+    }, 
+});
+
+gsap.to(".cloud-1", {
+    yPercent: -50,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".cloud-1",
+      // start: "top bottom", // the default values
+      // end: "bottom top",
+      scrub: true
+    }, 
+});
